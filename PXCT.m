34f9692244aa8749e7d9e137f3a28b1e2c2e57be
prese <prese@10.10.56.148>
2024-03-11 11:31:30 -0700

    crop, build, and visualize volumes, still with manual entry of metadata, alpha limits, etc.

--- PXCT.m
+++ PXCT.m
@@ -1,11 +1,11 @@
 % Visualize microfossil PXCT data
 % Ted Present, Feb 2024
 
-data_folder = "./PXCT_data/test" ;
+data_folder = "E:\PXCT\PXCT_data\test" ;
 tif_file_slice_list = dir(fullfile(data_folder,"*.tif"));
+firstTiff = Tiff(fullfile(data_folder,tif_file_slice_list(end).name));
 
 % convert tif slices to multipage tif volume
-firstTiff = Tiff(fullfile(data_folder,tif_file_slice_list(1).name));
 tag_struct.ImageLength = firstTiff.getTag("ImageLength"); %todo: loop through Tiff.getTagNames
 tag_struct.ImageWidth = firstTiff.getTag("ImageWidth");
 tag_struct.SampleFormat = firstTiff.getTag("SampleFormat");
@@ -19,33 +19,89 @@ tag_struct.XResolution = firstTiff.getTag("XResolution");
 tag_struct.YResolution = firstTiff.getTag("YResolution");
 tag_struct.Orientation = firstTiff.getTag("Orientation");
 
+%%
+firstTiffImg = read(firstTiff);
 
-tiff_vol = Tiff('testVol.tif', 'w8'); % prepare a BigTIFF for writing
+% s8_2 but not great crop...
+% xc = 1060;
+% yc = 1010;
+% r = 850;
+xc = 1040;
+yc = 1010;
+r = 750;
+
+figure;
+subplot(1,2,1)
+    imhist(firstTiffImg)
+subplot(1,2,2)
+    imshow(firstTiffImg)
+    viscircles([xc yc],r,'EdgeColor','r', 'LineWidth', 1);
+
+%%
+[xDim,yDim] = size(firstTiffImg);
+[xx,yy] = meshgrid(1:yDim,1:xDim);
+mask = false(xDim,yDim);
+mask = mask | hypot(xx - xc, yy - yc) <= r;
+mask = uint16(mask);
+
+croppedImg = firstTiffImg.*mask;
+croppedImg = imcrop(croppedImg,[xc-r yc-r 2.*r 2.*r]);
+
+imshow(croppedImg)
+
+%%
+% overwrite tag_struct with file-specific metadata
+tag_struct.ImageLength = size(croppedImg,1);
+tag_struct.ImageWidth = size(croppedImg,2);
+tag_struct.ResolutionUnit = 3; % cm
+tag_struct.XResolution = 3.484412e-06; % todo: read in metadata file
+tag_struct.YResolution = tag_struct.XResolution;
+
+tiff_vol = Tiff('testCroppedVol.tif', 'w8'); % prepare a BigTIFF for writing
 for i = 1:length(tif_file_slice_list)
     ith_tiff = Tiff(fullfile(data_folder,tif_file_slice_list(i).name));
+    ith_tiffImg = read(ith_tiff);
+    ith_tiffImg = ith_tiffImg.*mask;
+    ith_tiffImg = imcrop(ith_tiffImg,[xc-r yc-r 2.*r 2.*r]);
     setTag(tiff_vol,tag_struct);
-    write(tiff_vol,read(ith_tiff));
+    write(tiff_vol,ith_tiffImg);
     writeDirectory(tiff_vol);
 end
 close(tiff_vol)
-
-
-%%
-bim = blockedImage(tiffreadVolume('S4_2.tif'), BlockSize=[500 500 500]);
+clear tiff_vol;
 
 %%
-imhist(read(firstTiff))
+%reducedImg = imresize3(tiffreadVolume('S4_2_cropped.tif'), 0.25);
+%%
 
+bim = blockedImage(tiffreadVolume('testCroppedVol.tif'), BlockSize=[300 300 300]);
+
+imhist(tiffreadVolume('testCroppedVol.tif'));
+
+%%
 voldisp = volshow(bim);
 
-%voldisp.DataLimits(1) = 14000; % crop out low-count edges and pores
-%voldisp.DataLimits = [14000 17000]; % organics?
-voldisp.DataLimits = [20000 25000]; % chert?
-voldisp.Alphamap(end) = 0;  
+%%
+alpha = [0 0 1 1 0.002 0.002];
+color = [0 0 0;
+        200 140 75;
+        231 208 141;
+        231 208 141;
+        255 255 255;
+        255 255 255] ./ 255;
+% intensity = [0 11300 11300.1 11800 11801 40000]; % S4_2
+% intensity = [0 13500 13500.1 18000 18001 40000]; % S8_1
+% intensity = [0 15500 15500.1 21500 21501 40000]; % S8_2
+intensity = [0 14000 14000.1 19000 19000.1 25000]; % test
+queryPoints = linspace(min(intensity),max(intensity),256);
+alphamap = interp1(intensity,alpha,queryPoints)';
+colormap = interp1(intensity,color,queryPoints);
+
+voldisp.Alphamap = alphamap;
+voldisp.Colormap = colormap;
 
 %voldisp.RenderingStyle ="GradientOpacity";
-%voldisp.Alphamap(1:100) = 0;
 
-
+%%
+% use DataReadFinished event to record a video...
 %% functions
-
